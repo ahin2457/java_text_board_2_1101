a40e860f0ef2735c4f464fe58ab80239771be5cf
ahin2457 <dkdls2457@gmail.com>
2024-11-01 16:21:49 +0900

    형변환 integer.parseInt 사용

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -2,6 +2,34 @@ package com.sbs.exam.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   String queryString = "a=1&b=2&c=3";
+   String[] queryStringBits = queryString.split("&");
+
+   int a = 0;
+   int b = 0;
+   int c = 0;
+
+   for(String bit : queryStringBits){
+     //System.out.println(bit);
+     String[] bitBits = bit.split("=");
+     String paramName = bitBits[0];
+     String paramValue = bitBits[1];
+
+
+     if(paramName.equals("a")){
+       // 형변환 Integer.parseInt
+       a = Integer.parseInt(paramValue);
+     }
+     else if(paramName.equals("b")){
+       b = Integer.parseInt(paramValue);
+     }
+     else if(paramName.equals("c")){
+       c = Integer.parseInt(paramValue);
+     }
+     System.out.printf("%s : %s\n",paramName,paramValue);
+   }
+    System.out.printf("a : %d\n", a);
+    System.out.printf("b : %d\n", b);
+    System.out.printf("c : %d\n", c);
   }
 }

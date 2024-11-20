22fd30e8de1961a49ff80f9a1491901be1544d06
ahin2457 <dkdls2457@gmail.com>
2024-11-20 18:01:40 +0900

    쿼리스트링 파싱을 리스트가 아닌 맵에 넣기

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -1,34 +1,38 @@
 package com.sbs.exam.board;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
 
     //압축 된 데이터
-    String queryString = "count=1&id=33&age=14&studentNo=65";
+    String queryString = "id=33&hitCount=1&age=14&studentNo=65&name=Bob";
 
    // 압축 해제 시작 (데이터 파싱)
    String[] queryStringBits = queryString.split("&");
 
-    List<String> paramNames = new ArrayList<>();
-    List<Integer> paramValues = new ArrayList<>();
+   Map<String,String> params = new HashMap<>(); // 순서를 보장x
+    //Map<String,String> params = new LinkedHashMap<>(); // 순서를 보장o
 
-   for(String bit : queryStringBits){
+    for(String bit : queryStringBits){
      String[] bitBits = bit.split("=");
 
-     paramNames.add(bitBits[0]);
-     paramValues.add(Integer.parseInt(bitBits[1]));
+     params.put(bitBits[0],bitBits[1]);
    }
    // 압축 해제 끝
 
-    System.out.println(paramNames);
-    System.out.println(paramValues);
+    System.out.println("==원하는 것을 하나하나 가져와서 사용 ==");
+    System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
+    System.out.printf("hitCount : %d\n", Integer.parseInt(params.get("hitCount")));
+    System.out.printf("age : %d\n", Integer.parseInt(params.get("age")));
+    System.out.printf("studentNo : %d\n", Integer.parseInt(params.get("studentNo")));
+    System.out.printf("name : %s\n", params.get("name"));
 
-    // indexOf 함수 : id가 몇번째 위치하는지 찾는 함수
-    int findIndex = paramNames.indexOf("id");
-    System.out.printf ("id : %d\n", paramValues.get(findIndex));
+    System.out.println("== 반복문을 이용한 전체 출력 ==");
+    for(String paramName :  params.keySet()){
+      String paramValue = params.get(paramName);
+      System.out.printf("%s : %s\n", paramName,paramValue);
+    }
 
+    }
   }
-}

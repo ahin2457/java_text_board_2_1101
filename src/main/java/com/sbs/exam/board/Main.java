60080408b071edf324ba75880d3954f6c44c4d6e
ahin2457 <dkdls2457@gmail.com>
2024-11-20 17:14:12 +0900

    List를 도입하여 파라미터 데이터를 유연하게 다루도록 로직 수정

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -1,44 +1,28 @@
 package com.sbs.exam.board;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
    String queryString = "a=10&b=20&c=30&d=40";
    String[] queryStringBits = queryString.split("&");
 
-    // 문제점 : 로직 자체 유연하지 못한다.
-    // 왜? : 파라미터가 추가되면 그거에 맞는 로직을 다시 수정해줘야 하기 때문에
-
-   int a = 0;
-   int b = 0;
-   int c = 0;
-   int d = 0;
+    List<String> paramNames = new ArrayList<>();
+    List<Integer> paramValues = new ArrayList<>();
 
    for(String bit : queryStringBits){
-     //System.out.println(bit);
      String[] bitBits = bit.split("=");
-     String paramName = bitBits[0];
-     String paramValue = bitBits[1];
 
-
-     if(paramName.equals("a")){
-       // 문자열을 정수로 변환해줌. 형변환 Integer.parseInt
-       // 일반 int는 일반 타입 integer.parseInt는 객체 타입 그리고 null을 허용 (문자열인 String도 null을 허용)
-       a = Integer.parseInt(paramValue);
-     }
-     else if(paramName.equals("b")){
-       b = Integer.parseInt(paramValue);
-     }
-     else if(paramName.equals("c")){
-       c = Integer.parseInt(paramValue);
-     }
-     else if(paramName.equals("d")){
-       d = Integer.parseInt(paramValue);
-     }
-   //  System.out.printf("%s : %s\n",paramName,paramValue);
+     paramNames.add(bitBits[0]);
+     paramValues.add(Integer.parseInt(bitBits[1]));
    }
-    System.out.printf("a : %d\n", a);
-    System.out.printf("b : %d\n", b);
-    System.out.printf("c : %d\n", c);
-    System.out.printf("d : %d\n", d);
+
+   for(int i = 0; i < paramNames.size(); i++){
+    String paramName = paramNames.get(i);
+    int paramValue = paramValues.get(i);
+
+     System.out.printf("%s : %d\n", paramName,paramValue);
+   }
   }
 }
